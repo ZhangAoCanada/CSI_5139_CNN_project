2faddddaf6042282b6e63fec603925bb0dcf0464
ZhangAoCanada <zhangaocanada@gmail.com>
2019-10-24 13:02:22 -0400

    update

--- src/model/custom_segnet/model.py
+++ src/model/custom_segnet/model.py
@@ -85,16 +85,67 @@ class ConvSegNet:
         x = self.LastLayer(x, 32, 3)
         return x
 
+    def SegPred(self):
+        seg_out = self.ConvSegBody()
+        seg_pred = tf.sigmoid(seg_out)
+        return seg_pred
+
+    def RegularLoss(self):
+        pred = self.ConvSegBody()
+        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.Y, logits=pred))
+        return loss
+
     def WeightLoss(self):
         pred = self.ConvSegBody()
-        loss = tf.sigmoid(pred)
         Y_all = tf.reshape(self.Y, [-1,])
         Y_fg = tf.boolean_mask(self.Y, tf.cast(self.Y, tf.bool))
         pred_fg = tf.boolean_mask(pred, tf.cast(self.Y, tf.bool))
         Y_bg = tf.boolean_mask(self.Y, tf.cast(1 - self.Y, tf.bool))
         pred_bg = tf.boolean_mask(pred, tf.cast(1 - self.Y, tf.bool))
-        alpha = tf.shape(Y_bg)[0] / tf.shape(Y_all)[0]
-        return alpha
+        alpha = tf.cast(tf.shape(Y_bg)[0], tf.float32) / (tf.cast(tf.shape(Y_all)[0], tf.float32) \
+                                                            + tf.constant([1e-10], dtype=tf.float32))
+        loss_fg = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y_fg, logits=pred_fg))
+        loss_bg = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y_bg, logits=pred_bg))
+
+        loss_bg = tf.cond(tf.less(alpha[0], 1e-3), lambda: tf.zeros_like(loss_bg), lambda: loss_bg)
+        loss_fg = tf.cond(tf.greater(alpha[0], 1 - 1e-3), lambda: tf.zeros_like(loss_fg), lambda: loss_fg)
+        
+        loss = alpha * loss_fg + (1 - alpha) * loss_bg
+        return loss
+
+    def IoULoss(self):
+        # pred = self.ConvSegBody()
+        seg_pred = self.SegPred()
+        seg_pred_mask_float = tf.cast(tf.where(seg_pred > 0.5, tf.ones_like(seg_pred), \
+                                                tf.zeros_like(seg_pred)), tf.float32)
+        iou, _, _ = self.MaskIoU(seg_pred_mask_float, self.Y)
+        loss = 1 - iou
+        return loss
+
+    def MaskIoU(self, mask1, mask2):
+        mask1 = tf.reshape(mask1, [-1, ])
+        mask2 = tf.reshape(mask2, [-1, ])
+        mask1_bool = tf.cast(mask1, tf.bool)
+        mask2_bool = tf.cast(mask2, tf.bool)
+        intersection = tf.cast(tf.logical_and(mask1_bool, mask2_bool), tf.float32)
+        union = tf.cast(tf.logical_or(mask1_bool, mask2_bool), tf.float32)
+        iou = tf.reduce_sum(intersection) / (tf.reduce_sum(union) + 1e-10)
+        acc_mask1 = tf.reduce_sum(intersection) / (tf.reduce_sum(mask1) + 1e-10)
+        acc_mask2 = tf.reduce_sum(intersection) / (tf.reduce_sum(mask2) + 1e-10)
+        return iou, acc_mask1, acc_mask2
+
+    def Optimization(self, loss):
+        optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
+        learning_operation = optimizer.minimize(loss, global_step = self.global_step)
+        return learning_operation
+
+    def Metrics(self):
+        seg_pred = self.SegPred()
+        seg_pred_mask_float = tf.cast(tf.where(seg_pred > 0.5, tf.ones_like(seg_pred), \
+                                                tf.zeros_like(seg_pred)), tf.float32)
+        overall_iou, precision, recall = self.MaskIoU(seg_pred_mask_float, self.Y)
+        return overall_iou, precision, recall
+
 
 
         
--- src/model/custom_segnet/train.py
+++ src/model/custom_segnet/train.py
@@ -16,56 +16,113 @@ import tensorflow as tf
 import tensorflow.keras.layers as L
 from model import ConvSegNet
 
+class DataGenerator:
+    def __init__(self, input_dir, output_dir, input_size_ori, scale, batch_size):
+        self.input_size = input_size_ori
+        self.scale = scale
+        self.batch_size = batch_size
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+        self.all_imgs = glob(input_dir + "*.png")
+        self.all_labels = glob(output_dir + "*.npy")
+
+    def ChangeSize(self, gt):
+        gt = cv2.resize(gt, (gt.shape[1]//self.scale, gt.shape[0]//self.scale))
+        w, h = gt.shape
+        # get the final input width and height
+        input_w, input_h = self.input_size
+        input_w = input_w // self.scale
+        input_h = input_h // self.scale
+        # get zero paddings sizes
+        w_add = input_w - w
+        h_add = input_h - h
+        w_left = w_add // 2
+        h_top = h_add // 2
+        gt_new = np.zeros((input_w, input_h)).astype(np.float32)
+        gt_new[w_left:w_left+w, h_top:h_top+h] = gt
+        return gt_new
+
+    def GetInputGt(self, img_name, gt_name):
+        gt = np.load(gt_name).astype(np.float32)
+        gt = self.ChangeSize(gt)
+        gt = np.expand_dims(gt, axis = -1)
+        gt = np.expand_dims(gt, axis = 0)
+
+        img = np.array(Image.open(img_name)).astype(np.float32)
+        img = self.ChangeSize(img)
+        img = np.expand_dims(img, axis = -1)
+        img = np.expand_dims(img, axis = 0)
+        return img, gt
+
+    def GetBatchData(self):
+        all_index = np.arange(len(self.all_imgs))
+        np.random.shuffle(all_index)
+        num_batches = len(all_index) // self.batch_size
+        for batch_i in range(num_batches):
+            batch_index = all_index[batch_i*self.batch_size: (batch_i+1)*self.batch_size]
+            batch_imgs = []
+            batch_labels = []
+            for ind in batch_index:
+                img_name = self.input_dir + str(ind) + ".png"
+                gt_name = self.output_dir + str(ind) + ".npy"
+                img, gt = self.GetInputGt(img_name, gt_name)
+                batch_imgs.append(img)
+                batch_labels.append(gt)
+            batch_imgs = np.concatenate(batch_imgs, axis = 0)
+            batch_labels = np.concatenate(batch_labels, axis = 0)
+            yield batch_imgs, batch_labels
+
+
+batch_size = 2
+epoches = 100
 input_size_orig = (384, 1280)
 scale = 2
+model_input_size = (input_size_orig[0]//scale, input_size_orig[1]//scale)
+input_dir = "../../data_processing/train_in/"
+output_dir = "../../data_processing/train_out/"
 
-input_size = (input_size_orig[0]//scale, input_size_orig[1]//scale)
+dataGo = DataGenerator(input_dir, output_dir, input_size_orig, scale, batch_size)
 
-# def ChangeImageSize(img, input_size, scale):
-#     """
-#     Function:
-#         Get the input image into the standard size for the input of the model.
-#     """
-#     # resize the image according to the scale
-#     img = np.array(img.resize((img.size[0]//scale, img.size[1]//scale)))
-#     # get the resized image shape
-#     w, h = img.shape
-#     # get the final input width and height
-#     input_w, input_h = input_size
-#     input_w = input_w // scale
-#     input_h = input_h // scale
-#     # get zero paddings sizes
-#     w_add = input_w - w
-#     h_add = input_h - h
-#     w_left = w_add // 2
-#     h_top = h_add // 2
-#     # pad the image to the standard size
-#     img_new = np.zeros((input_w, input_h)).astype(np.float32)
-#     img_new[w_left:w_left+w, h_top:h_top+h] = img
-#     return img_new
+convsegModel = ConvSegNet(model_input_size)
 
-def ChangeSize(gt, input_size, scale):
-    """
-    Function:
-        Get the input image into the standard size for the input of the model.
-    """
-    gt = cv2.resize(gt, (gt.shape[1]//2, gt.shape[0]//2))
-    w, h = gt.shape
-    # get the final input width and height
-    input_w, input_h = input_size
-    input_w = input_w // scale
-    input_h = input_h // scale
-    # get zero paddings sizes
-    w_add = input_w - w
-    h_add = input_h - h
-    w_left = w_add // 2
-    h_top = h_add // 2
-    gt_new = np.zeros((input_w, input_h)).astype(np.float32)
-    gt_new[w_left:w_left+w, h_top:h_top+h] = gt
-    return gt_new
+t1 = convsegModel.ConvSegBody()
+# l = convsegModel.RegularLoss()
+# l = convsegModel.WeightLoss()
+l = convsegModel.IoULoss()
 
-all_ims = glob("../../data_processing/train_in/20.png")
-all_las = glob("../../data_processing/train_out/20.npy")
+# initialization
+init = tf.global_variables_initializer()
+
+# GPU settings
+gpu_options = tf.GPUOptions(allow_growth=True)
+
+with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+    sess.run(init)
+    for batch_imgs, batch_gts in dataGo.GetBatchData():
+        test1 = sess.run(t1, feed_dict = {convsegModel.X: batch_imgs,
+                                            convsegModel.Y: batch_gts})
+        print(test1.shape)
+
+
+
+
+###############################################################
+# i = 60
+# img_name = "../../data_processing/train_in/" + str(i) + ".png"
+# gt_name = "../../data_processing/train_out/" + str(i) + ".npy"
+# img, gt = GetInputGt(img_name, gt_name)
+
+# img = np.squeeze(img)
+# gt = np.squeeze(gt)
+
+# fig = plt.figure()
+# ax1 = fig.add_subplot(211)
+# ax2 = fig.add_subplot(212)
+# ax1.imshow(img)
+# ax2.imshow(gt)
+# plt.show()
+###############################################################
+
 
 ###############################################################
 # plt.ion()
@@ -86,31 +143,3 @@ all_las = glob("../../data_processing/train_out/20.npy")
 #     fig.canvas.draw()
 #     plt.pause(0.3)
 ###############################################################
-
-gt = np.load(all_las[0]).astype(np.float32)
-gt = ChangeSize(gt, input_size_orig, scale)
-gt = np.expand_dims(gt, axis = -1)
-gt = np.expand_dims(gt, axis = 0)
-
-img = np.array(Image.open(all_ims[0])).astype(np.float32)
-img = ChangeSize(img, input_size_orig, scale)
-
-img = np.expand_dims(img, axis = -1)
-img = np.expand_dims(img, axis = 0)
-
-convsegModel = ConvSegNet(input_size)
-
-t1 = convsegModel.ConvSegBody()
-l1 = convsegModel.WeightLoss()
-
-# initialization
-init = tf.global_variables_initializer()
-
-# GPU settings
-gpu_options = tf.GPUOptions(allow_growth=True)
-
-with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-    sess.run(init)
-    test1 = sess.run(l1, feed_dict = {convsegModel.X: img,
-                                        convsegModel.Y: gt})
-    print(test1)
